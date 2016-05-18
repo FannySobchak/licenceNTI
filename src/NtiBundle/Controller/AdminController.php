12366afee421de4d5cfc167ed7149a15de568fe5
Anthony Dardenne <karterhns@gmail.com>
2016-05-18 15:47:36 +0200

    Fix du lien de l'onglet Admin

--- src/NtiBundle/Controller/AdminController.php
+++ src/NtiBundle/Controller/AdminController.php
@@ -10,7 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 /**
  * Admin controller.
  *
- * @Route("/admin/")
+ * @Route("/admin")
  */
 class AdminController extends Controller
 {
