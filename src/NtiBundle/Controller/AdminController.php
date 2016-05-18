da27d72075fabc73afa8cfbbf8b1a7636d938393
Alain DRILLON <contact@drillon-ala.in>
2016-05-18 08:46:38 +0200

    Ajout de la page d'accueil de l'administration

---
+++ src/NtiBundle/Controller/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NtiBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Admin controller.
+ *
+ * @Route("/admin/")
+ */
+class AdminController extends Controller
+{
+	/**
+     * @Route("/", name="admin_index")
+	 */
+    public function indexAction()
+    {
+        return $this->render('NtiBundle:Admin:index.html.twig');
+	}
+}
