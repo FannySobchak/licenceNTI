987a360f0b3a9d6e844f0462c16366757fa09137
Fanny B <Fanny B>
2016-05-25 21:01:26 +0200

    1er tentative de upload fichier

---
+++ src/NtiBundle/Form/FichierType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace NtiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FichierType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('chemin')
+            ->add('path')
+            ->add('user')
+            ->add('cours')
+            ->add('file')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'NtiBundle\Entity\Fichier'
+        ));
+    }
+}
